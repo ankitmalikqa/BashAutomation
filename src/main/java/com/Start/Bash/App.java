89d64093b6a2688af6c58b571506a64c0ed82f74
Ankit Malik <ankitmalik@qait.com>
2020-09-18 18:28:48 +0530

    malik

--- src/main/java/com/Start/Bash/App.java
+++ src/main/java/com/Start/Bash/App.java
@@ -3,6 +3,7 @@ package com.Start.Bash;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 
 /**
  * Hello world!
@@ -12,8 +13,11 @@ public class App
 {
     public static void main( String[] args ) throws IOException, InterruptedException
     {
+    	Scanner scan=new Scanner(System.in);
+    	String message=scan.next();
+    	scan.close();
           		ProcessBuilder builder = new ProcessBuilder(
-    	            "cmd.exe", "/c", "pushCode.sh 'ashoka'  && dir");
+    	            "cmd.exe", "/c", "pushCode.sh '"+message+"'  && dir");
     	        builder.redirectErrorStream(true);
     	        Process p = builder.start();
     	        BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
